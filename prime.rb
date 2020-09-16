5d3c72740b55b440ca7edf3c957535939140a113
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-16 21:52:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+  x = 2
+    if number > 1
+      range = (x..number-1).to_a
+      range.none? do |test|
+        number % test == 0
+      end
+        else false
+    end
+end
